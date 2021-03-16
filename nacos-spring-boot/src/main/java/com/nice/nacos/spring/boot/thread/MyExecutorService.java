55199cd736290432d652e1ea3feef61c5c17efbc
nice <744709409@qq.com>
2021-03-16 18:43:55 +0800

    新建项目

--- nacos-spring-boot/src/main/java/com/nice/nacos/spring/boot/thread/MyExecutorService.java
+++ nacos-spring-boot/src/main/java/com/nice/nacos/spring/boot/thread/MyExecutorService.java
@@ -1,8 +1,6 @@
 package com.nice.nacos.spring.boot.thread;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
